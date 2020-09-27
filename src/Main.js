647380d72202f8baeec566906bd5f2b0389bafa1
maxhs2014 <maxhs2014@yahoo.com>
2020-09-27 17:10:15 -0400

    done

--- src/Main.js
+++ src/Main.js
@@ -9,58 +9,113 @@ var client = xmlrpc.createClient({ host: 'localhost', port: 9090, path: '/RPC2'}
 export default class Main extends React.Component {
   state = {
     value: 0,
-    labels: [],
-  datasets: [
-    {
-      label: 'Predictions',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderWidth: 2,
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: []
+    goodVal: 0,
+    graph1: {
+      labels: [],
+      datasets: [
+        {
+          label: 'Predictions',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderWidth: 2,
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: []
+        },
+        {
+          label: 'Trues',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: '#DB3A34',
+          borderColor: '#DB3A34',
+          borderWidth: 2,
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: '#DB3A34',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: '#DB3A34',
+          pointHoverBorderColor: '#DB3A34',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: []
+        }
+      ]
     },
-    {
-      label: 'Trues',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: '#DB3A34',
-      borderColor: '#DB3A34',
-      borderWidth: 2,
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: '#DB3A34',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: '#DB3A34',
-      pointHoverBorderColor: '#DB3A34',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: []
-    }
-  ]
+    graph2: {
+      labels: [],
+      datasets: [
+        {
+          label: 'Predictions',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderWidth: 2,
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: []
+        },
+        {
+          label: 'Trues',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: '#DB3A34',
+          borderColor: '#DB3A34',
+          borderWidth: 2,
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: '#DB3A34',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: '#DB3A34',
+          pointHoverBorderColor: '#DB3A34',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: []
+        }
+      ]
+    },
   };
 
   componentDidMount() {
     this.getPrediction()
+    this.getCurrentPrediction()
     this.getManyPrediction()
+    this.getGoodPrediction()
+    
   }
 
   handleClick = buttonName => {
@@ -79,17 +134,46 @@ export default class Main extends React.Component {
     })
   }
 
+  getCurrentPrediction = () => {
+    client.methodCall('predictCurrentGood', ["2020-9-27"], (error, val) => {
+      // Results of the method response
+      
+      console.log('Method response for \'anAction\': ' + val)
+      val = val *1000
+      val = Math.round(val)
+      val = val/1000
+      this.setState({goodVal: val})
+    })
+  }
+
   getManyPrediction = () => {
     client.methodCall('predictMany', [], (error, val) => {
       // Results of the method response
       
       console.log('Method response for \'anAction\': ' + val)
-      var data = this.state.datasets
-      data[0].data = val[1]
-      data[1].data = val[2]
-      data[0].data.push(this.state.value)
+      var data = this.state.graph1
+      data.labels = val[0]
+      data.datasets[0].data = val[1]
+      data.datasets[1].data = val[2]
+      data.datasets[0].data.push(this.state.value)
       val[0].push("2020-9-27")
-      this.setState({labels: val[0], datasets: data})
+      this.setState({graph1: data})
+      console.log( val[0])
+    })
+  }
+
+  getGoodPrediction = () => {
+    client.methodCall('predictGood', [], (error, val) => {
+      // Results of the method response
+      
+      console.log('Method response for \'anAction\': ' + val)
+      var data = this.state.graph2
+      data.labels = val[0]
+      data.datasets[0].data = val[1]
+      data.datasets[1].data = val[2]
+      data.datasets[0].data.push(this.state.goodVal)
+      val[0].push("2020-9-27")
+      this.setState({graph2: data})
       console.log( val[0])
     })
   }
@@ -105,11 +189,26 @@ export default class Main extends React.Component {
                 <header className="App-header">
                 <Line
                 className="graph"
-          data={this.state}
+          data={this.state.graph1}
           options={{
             title:{
               display:true,
-              text:'',
+              text:'Predictions Based on Covid Data',
+              fontSize:20
+            },
+            legend:{
+              display:true,
+              position:'right'
+            }
+          }}
+        />
+        <Line
+                className="graph"
+          data={this.state.graph2}
+          options={{
+            title:{
+              display:true,
+              text:'Predictions Based on Covid Data and Previous Stock Market Data',
               fontSize:20
             },
             legend:{
@@ -119,7 +218,7 @@ export default class Main extends React.Component {
           }}
         />
                 <p>
-                    Tomorrow's S&P 500 at close will be {this.state.value}
+                    Tomorrow's S&P 500 at close will be {this.state.value} based on Covid Data and {this.state.goodVal} based on Covid and Stock Marked Data
                 </p>
                 </header>
             </div>
